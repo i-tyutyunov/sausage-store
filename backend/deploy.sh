df93b27ca1f25fcfe6cf1cb66fb761bcb29aade3
igor_tyutyunov <tyutyunov@braind.agency>
2024-02-04 21:57:04 +0300

    Копирование unit-а

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,13 +1,11 @@
 # /bin/bash
 set -xe
-sudo mkdir -p /opt/sausage-store/bin/current-version
-sudo rm -rf /opt/sausage-store/bin/current-version/*
 
-cd /opt/sausage-store/bin/current-version
-sudo curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-${VERSION}.jar \
+sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
+
+cd /opt/sausage-store/bin
+sudo curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar \
 ${NEXUS_REPO_URL}/repository/${NEXUS_REPO_BACKEND_NAME}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
-
-sudo ln -sf /opt/sausage-store/bin/current-version/sausage-store-${VERSION}.jar /opt/sausage-store/bin/sausage-store.jar
 
 sudo systemctl daemon-reload
 sudo systemctl restart sausage-store-backend
