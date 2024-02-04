01421b95e2ca64e513b7b7e73760e7b457e18272
igor_tyutyunov <tyutyunov@braind.agency>
2024-02-04 20:30:59 +0300

    Добавил переменных

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -4,7 +4,8 @@ sudo mkdir -p /opt/sausage-store/bin/current-version
 sudo rm -rf /opt/sausage-store/bin/current-version/*
 
 cd /opt/sausage-store/bin/current-version
-sudo curl -u std-025-75:7q2g4NSw -o sausage-store-${VERSION}.jar https://nexus.praktikum-services.tech/repository/std-025-75-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
+sudo curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-${VERSION}.jar \
+${NEXUS_REPO_URL}/repository/${NEXUS_REPO_BACKEND_NAME}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
 
 sudo ln -sf /opt/sausage-store/bin/current-version/sausage-store-${VERSION}.jar /opt/sausage-store/bin/sausage-store.jar
 
--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,5 +1,6 @@
 # /bin/bash
 
-curl -u std-025-75:7q2g4NSw -o sausage-store-${VERSION}.tar.gz https://nexus.praktikum-services.tech/repository/std-025-75-frontend/${VERSION}/sausage-store-${VERSION}.tar.gz
+curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-${VERSION}.tar.gz \
+${NEXUS_REPO_URL}/repository/${NEXUS_REPO_FRONTEND_NAME}/${VERSION}/sausage-store-${VERSION}.tar.gz
 sudo tar -C "/opt/sausage-store/static/dist/" -xvf sausage-store-${VERSION}.tar.gz
 rm -f sausage-store-${VERSION}.tar.gz
