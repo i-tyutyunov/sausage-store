59c16995f16565ec6464db2a3d879385f3aeccfc
igor_tyutyunov <tyutyunov@braind.agency>
2024-03-05 20:04:19 +0300

    Добавление сертификата при деплое

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,5 +1,21 @@
 # /bin/bash
 set -xe
+
+check_certificate_existence() {
+    keytool -list -keystore /etc/ssl/certs/java/cacerts -alias yandex -storepass changeit >/dev/null 2>&1
+    return $?
+}
+
+if ! check_certificate_existence; then
+  wget "https://storage.yandexcloud.net/cloud-certs/CA.pem" -O ~/YandexInternalRootCA.crt
+  sudo keytool -importcert \
+               -file ~/YandexInternalRootCA.crt \
+               -alias yandex \
+               -cacerts \
+               -storepass changeit \
+               -noprompt
+fi
+
 sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
 
 cd /opt/sausage-store/bin
