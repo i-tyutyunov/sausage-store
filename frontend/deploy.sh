54a93c9b7b744b6054be99e64f640b0130b3c9e3
igor_tyutyunov <tyutyunov@braind.agency>
2024-03-12 22:20:43 +0300

    frontend docker ci

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,6 +1,21 @@
-# /bin/bash
+#! /bin/bash
+set -xe
 
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-${VERSION}.tar.gz \
-${NEXUS_REPO_URL}/repository/${NEXUS_REPO_FRONTEND_NAME}/${VERSION}/sausage-store-${VERSION}.tar.gz
-sudo tar -C "/opt/sausage-store/static/dist/" -xvf sausage-store-${VERSION}.tar.gz
-rm -f sausage-store-${VERSION}.tar.gz
+is_backend_running() {
+    docker ps -q --filter "name=backend"
+}
+
+while ! is_backend_running; do
+    echo "Backend is not running yet, waiting..."
+    sleep 1
+done
+
+echo "Backend is running, starting frontend..."
+
+sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
+sudo docker network create -d bridge sausage_network || true
+sudo docker rm -f sausage-frontend || true
+sudo docker run --rm -d --name sausage-frontend \
+     --network=sausage_network \
+     -v /tmp/${CI_PROJECT_DIR}/frontend/default.conf:/etc/nginx/conf.d/default.conf \
+     "${CI_REGISTRY_IMAGE}"/sausage-frontend:latest
