8cf6bd042f91af81e7a2608ee8a6e77931992c19
igor_tyutyunov <tyutyunov@braind.agency>
2024-03-12 23:34:55 +0300

    Тест

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -16,7 +16,7 @@ sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY
 sudo docker network create -d bridge sausage_network || true
 sudo docker rm -f sausage-frontend || true
 sudo docker run -d --name sausage-frontend \
-     -v /tmp/${CI_PROJECT_DIR}/frontend/default.conf:/etc/nginx/conf.d/default.conf \
+     -v /home/student/example-02/frontend/default.conf:/etc/nginx/conf.d/default.conf \
      -p 9000:80 \
      --restart=always  \
      --network=sausage_network \
