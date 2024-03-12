f6852225c4829f0689f44f55abc915f0f52d3e0f
igor_tyutyunov <tyutyunov@braind.agency>
2024-03-13 00:14:33 +0300

    Тест

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -15,9 +15,11 @@ echo "Backend is running, starting frontend..."
 sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker network create -d bridge sausage_network || true
 sudo docker rm -f sausage-frontend || true
+sudo echo ${CI_PROJECT_DIR} > ~/CI_PROJECT_DIR.env
+sudo echo ${CI_REGISTRY_IMAGE} > ~/CI_REGISTRY_IMAGE.env
 sudo docker run -d --name sausage-frontend \
-     -v /tmp${CI_PROJECT_DIR}/frontend/default.conf:/etc/nginx/conf.d/default.conf \
+     -v /tmp/builds/std-025-75/sausage-store/frontend/default.conf:/etc/nginx/conf.d/default.conf \
      -p 9000:80 \
      --restart=always  \
      --network=sausage_network \
-     "${CI_REGISTRY_IMAGE}"/sausage-frontend:latest
+     gitlab.praktikum-services.ru:5050/std-025-75/sausage-store/sausage-frontend:latest
