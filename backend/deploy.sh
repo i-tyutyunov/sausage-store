6313255aebccc11b911696c44178510f20d9d03a
igor_tyutyunov <tyutyunov@braind.agency>
2024-03-31 23:54:55 +0300

    test2

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,6 +1,11 @@
 #/bin/bash
 
 NUM_INSTANCES=1
+
+echo "123"
+echo $NUM_INSTANCES
+echo "321"
+exit
 APP_NAME="sausage-store-backend"
 
 echo "Determine the current version of the application (blue or green)"
